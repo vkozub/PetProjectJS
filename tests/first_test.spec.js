666346b8c1d42ed17f0573c3e417e3d5681b6515
vkozub <vsevolod.kozub@gmail.com>
2023-11-03 10:43:37 -0400

    first commit

---
+++ tests/first_test.spec.js
@@ -0,0 +1,11 @@
+// import { test, expect, FakerObject } from './support/env.js'
+const { test, expect, faker } = require('./support/env.js');
+// import { faker } from '@faker-js/faker';
+
+
+test('has title 2', async ({ page }) => {
+    await page.goto(process.env.UI_ENV);
+  console.log(faker.internet.email());
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/);
+  });
---
+++ tests/support/env.js
@@ -0,0 +1,10 @@
+// import 'dotenv/config'
+require('dotenv/config')
+
+process.env.UI_ENV = `https://${process.env.NAMESPACE}.${process.env.CLUSTER}/`;
+console.log(process.env);
+const { test, expect } = require('@playwright/test');
+// export const { test, expect } = require('@playwright/test');
+const { faker } = require('@faker-js/faker');
+// export const { faker: FakerObject } = require('@faker-js/faker');
+module.exports = { test, expect, faker };
