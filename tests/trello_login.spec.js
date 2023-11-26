f247cffec2f1c53ad391b9669910925d0558d73b
vkozub <vsevolod.kozub@gmail.com>
2023-11-26 21:01:35 +0200

    update

--- tests/trello_login.spec.js
+++ tests/trello_login.spec.js
@@ -4,8 +4,8 @@ const { test, expect } = require('./../pages/pageFixtures.js');
 test('log in Trello', async ({ loginHomePage, loginPage, page }) => {
     await page.goto(process.env.UI_ENV);
     await loginHomePage.tapLogIn();
-    await loginPage.putUsername('username');
-    await loginPage.putPassword('password');
+    await loginPage.putUsername(process.env.TRELLO_USERNAME);
+    await loginPage.putPassword(process.env.TRELLO_PASSWORD);
     await loginPage.tapLogIn();
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/Trello/);
