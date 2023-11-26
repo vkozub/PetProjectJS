32b5e7b6d24e0d92c4c90191d67402607611fd0d
vkozub <vsevolod.kozub@gmail.com>
2023-11-26 21:29:07 +0200

    update

--- pages/LoginHomePage.js
+++ pages/LoginHomePage.js
@@ -1,4 +1,4 @@
-const BasePage = require('./BasePage');
+const BasePage = require('./BasePage.js');
 
 module.exports = class LoginHomePage extends BasePage {
     get logInButton() { return $('div > a[data-uuid*="login"]'); }
--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,4 +1,4 @@
-const BasePage = require('./BasePage');
+const BasePage = require('./BasePage.js');
 
 module.exports = class LoginPage extends BasePage {
     get usernameInput() { return $("username"); }
