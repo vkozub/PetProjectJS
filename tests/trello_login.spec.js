c065cf5e158673bfec42a89b846129da50e5adf7
vkozub <vsevolod.kozub@gmail.com>
2023-12-08 05:59:45 -0500

    add before and after hooks

--- tests/trello_login.spec.js
+++ tests/trello_login.spec.js
@@ -1,13 +1,22 @@
 const { faker } = require('./support/env.js');
 const { test } = require('./../pages/pageFixtures.js');
 
-test('Verify that member can do login', async ({ loginHomePage, loginPage, userBoardsPage }) => {
+test.describe('trello login', () => {
+  test.beforeEach(async ({ loginHomePage, loginPage }) => {
     await loginHomePage.visit();
     await loginHomePage.tapLogIn();
     await loginPage.putUsername(process.env.TRELLO_USERNAME);
     await loginPage.tapContinue();
     await loginPage.putPassword(process.env.TRELLO_PASSWORD);
     await loginPage.tapLogIn();
+  });
+
+  test('Verify that member can do login', async ({ userBoardsPage }) => {
     await userBoardsPage.verifyNavBarVisible();
     await userBoardsPage.verifyYourWorkspacesLabelVisible();
   });
+
+  test.afterEach(async ({ context }) => {
+    await context.close();
+  });
+});
