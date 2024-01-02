4138393cc7aa82cd37da919aae052d1be35ba5e1
vkozub <vsevolod.kozub@gmail.com>
2024-01-02 07:10:40 -0500

    add test create workspace

---
+++ tests/trello_create_workspace.spec.js
@@ -0,0 +1,21 @@
+const { faker } = require('./support/env.js');
+const { test } = require('./../pages/pageFixtures.js');
+
+test.describe('Trello Create Workspace', () => {
+  test.beforeEach(async ({ loginHomePage, loginPage }) => {
+    await loginHomePage.visit();
+    await loginHomePage.tapLogIn();
+    await loginPage.putUsername(process.env.TRELLO_USERNAME);
+    await loginPage.tapContinue();
+    await loginPage.putPassword(process.env.TRELLO_PASSWORD);
+    await loginPage.tapLogIn();
+  });
+
+  test('Verify that member can create a new workspace via UI', async ({ userBoardsPage }) => {
+    await userBoardsPage.verifyNavBarVisible();
+  });
+
+  test.afterEach(async ({ context }) => {
+    await context.close();
+  });
+});
