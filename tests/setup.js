caf8c34e0bc4689df9e9bf1269d40ee36c1be0cc
vkozub <vsevolod.kozub@gmail.com>
2024-01-22 05:59:56 -0500

    update setup

--- tests/setup.js
+++ tests/setup.js
@@ -2,10 +2,8 @@ const { test: setup } = require('./../support/env.js');
 
 setup('Trello setup', async ({ membersEndpoint }) => {
     // retrieve Trello member information
-    if (!process.env.TRELLO_MEMBER) {
     const memberTrello = await membersEndpoint.searchMember();
     let workspaceName = memberTrello.fullName + "'s workspace";
     memberTrello.defaultWorkspace = workspaceName;
     process.env.TRELLO_MEMBER = JSON.stringify(memberTrello);
-    };
 });
