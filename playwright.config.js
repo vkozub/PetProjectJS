282bc9c92b45ecbe3932e95f88d9470c81dec768
vkozub <vsevolod.kozub@gmail.com>
2024-02-02 06:07:20 -0500

    update

--- playwright.config.js
+++ playwright.config.js
@@ -21,6 +21,7 @@ module.exports = defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [ process.env.CI ? ['junit', { outputFile: 'test-results/results.xml' }] : ['html'] ],
+  // reporter: [ process.env.CI ? ['allure-playwright'] : ['html'] ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
