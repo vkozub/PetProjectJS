e2ada8751368319a28a2e1f2f94f96b2c15dc75a
vkozub <vsevolod.kozub@gmail.com>
2024-08-29 10:16:18 -0400

    implement tests to create a service via ui and api

--- tests/eHelper/createService.spec.js
+++ tests/eHelper/createService.spec.js
@@ -1,4 +1,4 @@
-const { test } = require('../../support/env.js');
+const { test, faker } = require('../../support/env.js');
 const base = require('@playwright/test');
 const expect = base.expect;
 const fs = require("fs");
@@ -23,13 +23,58 @@ test.describe('eHelper Create a service', () => {
     page = await context.newPage();
   });
 
-  test.skip('Verify that service can be created via UI', async ({}) => {
+  test('Verify that service can be created via UI', async ({}) => {
     await page.goto('/profile');
+
+    // Create a new service
+    const payload = { 
+      userId: user.email,
+      userName: user.userName,
+      countPosts: 1,
+      region: "Івано-Франківська область",
+      city: "Івано-Франківськ",
+      category: "Послуги масажу",
+      service: "Надаю послугу",
+      text: faker.lorem.sentence({ min: 6, max: 8 })
+    };
+
+    await page.locator('select.rounded-md').first().selectOption(payload.region);
+    await page.locator('select.rounded-md').nth(1).selectOption(payload.city);
+    await page.locator('select.rounded-md').nth(2).selectOption(payload.category);
+    await page.locator('select.rounded-md').last().selectOption(payload.service);
+    await page.locator('#areaId').fill(payload.text);
+    await expect(page.getByRole('button', { name: 'Опублікувати' })).toBeEnabled();
+    await page.getByRole('button', { name: 'Опублікувати' }).click();
+
+    await expect(page.locator("div[class='flex flex-col max-sm:w-80 min-w-80 sm:max-w-lg lg:max-w-3xl']")).toContainText(payload.text);
   });
 
-  test.skip('Verify that service can be created via API', async ({}) => {
+  test('Verify that service can be created via API', async ({}, testInfo) => {
+    // Create a new service
+    const payload = { 
+      userId: user.email,
+      userName: user.userName,
+      countPosts: 1,
+      region: "Івано-Франківська область",
+      city: "Івано-Франківськ",
+      category: "Послуги масажу",
+      service: "Надаю послугу",
+      text: faker.lorem.sentence()
+    };
+
+    const response = await page.request.post(testInfo.project.use.config.baseUrl + '/api/users/post', {
+        data: payload
+    });
+
     await page.goto('/');
-    await expect(page.locator("p[class*='text-gray']")).toHaveText(aboutUsText);
+    await expect.soft(page.locator("p[class*='text-sm']")).toContainText('Вітаємо, ' + user.userName + '.');
+    await page.locator('select.rounded-md').first().selectOption(payload.region);
+    await page.locator('select.rounded-md').nth(1).selectOption(payload.city);
+    await page.locator('select.rounded-md').nth(2).selectOption(payload.category);
+    await page.locator('select.rounded-md').last().selectOption(payload.service);
+    await page.getByRole('button', { name: 'Пошук' }).click();
+
+    await expect(page.locator("div[class='flex flex-col max-sm:w-80 min-w-80 sm:max-w-lg lg:max-w-3xl']")).toContainText(payload.text);
   });
 
   test.afterEach(async ({}) => {
