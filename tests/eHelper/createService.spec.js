a4760379f050679a67f24175ed060c726e176e0c
vkozub <vsevolod.kozub@gmail.com>
2024-08-29 10:38:24 -0400

    add post verification in DB

--- tests/eHelper/createService.spec.js
+++ tests/eHelper/createService.spec.js
@@ -23,30 +23,40 @@ test.describe('eHelper Create a service', () => {
     page = await context.newPage();
   });
 
-  test('Verify that service can be created via UI', async ({}) => {
+  test('Verify that service can be created via UI', async ({}, testInfo) => {
     await page.goto('/profile');
 
     // Create a new service
-    const payload = { 
-      userId: user.email,
-      userName: user.userName,
-      countPosts: 1,
+    const searchOptions = { 
       region: "Івано-Франківська область",
       city: "Івано-Франківськ",
       category: "Послуги масажу",
-      service: "Надаю послугу",
-      text: faker.lorem.sentence({ min: 6, max: 8 })
+      service: "Надаю послугу"
     };
 
-    await page.locator('select.rounded-md').first().selectOption(payload.region);
-    await page.locator('select.rounded-md').nth(1).selectOption(payload.city);
-    await page.locator('select.rounded-md').nth(2).selectOption(payload.category);
-    await page.locator('select.rounded-md').last().selectOption(payload.service);
-    await page.locator('#areaId').fill(payload.text);
+    const service = { text: faker.lorem.sentence({ min: 6, max: 8 }) };
+    Object.assign(service, searchOptions);
+
+    await page.locator('select.rounded-md').first().selectOption(service.region);
+    await page.locator('select.rounded-md').nth(1).selectOption(service.city);
+    await page.locator('select.rounded-md').nth(2).selectOption(service.category);
+    await page.locator('select.rounded-md').last().selectOption(service.service);
+    await page.locator('#areaId').fill(service.text);
     await expect(page.getByRole('button', { name: 'Опублікувати' })).toBeEnabled();
     await page.getByRole('button', { name: 'Опублікувати' }).click();
+    // Verify existing of the post on UI
+    await expect(page.locator("div[class='flex flex-col max-sm:w-80 min-w-80 sm:max-w-lg lg:max-w-3xl']")).toContainText(service.text);
 
-    await expect(page.locator("div[class='flex flex-col max-sm:w-80 min-w-80 sm:max-w-lg lg:max-w-3xl']")).toContainText(payload.text);
+    // Verify existing of the post in DB
+    const getPostsresponse = await page.request.post(testInfo.project.use.config.baseUrl + '/api/users/getposts', {
+      data: searchOptions
+    });
+
+    const responseJson = await getPostsresponse.json();
+    const posts = responseJson?.data;
+    const post = posts.find(post => post?.text === service.text);
+    expect(post).toBeTruthy();
+
   });
 
   test('Verify that service can be created via API', async ({}, testInfo) => {
